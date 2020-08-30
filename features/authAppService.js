687476bc5ec1ee7766b882516dbcd58965bc8e2d
Angello Antonelly <angelloantonelly@live.com>
2020-08-30 14:35:17 -0600

    Added create user method and added encrypting password logic

--- features/authAppService.js
+++ features/authAppService.js
@@ -1,13 +1,40 @@
 const { response } = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 // POST: api/auth/register/
 const createUser = async(req, res = response) => {
-    const userReq = req.body;
+    const { email, password } = req.body;
 
-    return res.status(201).json({
-        Message: 'createUser',
-        Data: userReq
-    });
+    try {
+        const isExistingUser = await User.findOne({ email: email });
+        if( isExistingUser ) {
+            return res.status(400).json({
+                Message: 'An user already exists with this e-mail.',
+                Data: null
+            });
+        }
+
+        const newUser = new User( req.body );
+
+        // encrypting password
+        const salt = bcrypt.genSaltSync();
+        newUser.password = bcrypt.hashSync( password, salt );
+
+        await newUser.save();
+
+        newUser.password = '*******';
+        return res.status(201).json({
+            Message: '',
+            Data: newUser
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            Message: 'This user could not be created.',
+            Data: null
+        });
+    }
 }
 
 // POST: api/auth/
