6c7b0a8dc25a24cd039751d34557d43a421f2c42
Angello Antonelly <angelloantonelly@live.com>
2020-09-07 23:30:20 -0600

    Calendar App Heroku deployment v1

---
+++ public/precache-manifest.0b0be3593d103870c3f10dacd811746c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62bcee67cc2524388afaaf5e630f511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3fa95230edc4f95b3e7",
+    "url": "/static/css/2.608a309a.chunk.css"
+  },
+  {
+    "revision": "09caa64ff84e1ab2e959",
+    "url": "/static/css/main.01d97cf2.chunk.css"
+  },
+  {
+    "revision": "d3fa95230edc4f95b3e7",
+    "url": "/static/js/2.a182eb24.chunk.js"
+  },
+  {
+    "revision": "7155aa3aaf2824ce502a909c4c5e2b94",
+    "url": "/static/js/2.a182eb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09caa64ff84e1ab2e959",
+    "url": "/static/js/main.7ab5848c.chunk.js"
+  },
+  {
+    "revision": "633e7b928768a04cb0b8",
+    "url": "/static/js/runtime-main.d1e319d9.js"
+  },
+  {
+    "revision": "f7193988fdaca15b1f89b84d5f5d8b46",
+    "url": "/static/media/calendar-medium.f7193988.jpg"
+  },
+  {
+    "revision": "987813c7ad259541c2942c5dfcb324e6",
+    "url": "/static/media/user.987813c7.svg"
+  }
+]);
